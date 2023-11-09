2d8148f14e0a0df42b8c8dc34f5a84f1f8ed0bdd
pgrdaw <pgrdaw@gmail.com>
2023-11-10 00:11:45 +0100

    incorporado metodo juegoFinalizado()

--- TresEnRaya.js
+++ TresEnRaya.js
@@ -10,27 +10,62 @@ function comprobarVacio(fila, columna, tablero) {
     }
 }
 
+function comprobarHorizontales(tablero) {
+    for (let f = 0; f <= 2; f++) {
+        if (tablero[f][0] != '·' &&
+            tablero[f][0] == tablero[f][1] &&
+            tablero[f][1] == tablero[f][2]) {
+            return true
+        }
+    }
+    return false
+}
+
+function comprobarVerticales(tablero) {
+    for (let c = 0; c <= 2; c++) {
+        if (tablero[0][c] != '·' &&
+            tablero[0][c] == tablero[1][c] &&
+            tablero[1][c] == tablero[2][c]) {
+            return true
+        }
+    }
+    return false
+}
+
+function comprobarDiagonales(tablero) {
+    for (let d = 0; d <= 2; d++) {
+        if (tablero[0][0] != '·' &&
+            tablero[0][0] == tablero[1][1] &&
+            tablero[1][1] == tablero[2][2]) {
+            return true
+        }
+        if (tablero[2][0] != '·' &&
+            tablero[2][0] == tablero[1][1] &&
+            tablero[1][1] == tablero[0][2]) {
+            return true
+        }
+    }
+    return false
+}
+
+
 class TresEnRaya {
 
     constructor() {
-        this.tablero = [
-            ['·', '·', '·'],
-            ['·', '·', '·'],
-            ['·', '·', '·']
-        ]
+        this.tablero = [['·', '·', '·'], ['·', '·', '·'], ['·', '·', '·']]
         this.jugador = 'O'
     }
 
     jugar(fila, columna) {
         comprobarLimites(fila, columna)
         comprobarVacio(fila, columna, this.tablero)
-
         this.tablero[fila][columna] = this.jugador
         this.turno()
+        this.finalizado()
     }
 
     turno() {
-        if (this.jugador = 'O') {
+        if (this.jugador === 'O') {
             this.jugador = 'X'
         } else {
             this.jugador = 'O'
@@ -47,6 +82,17 @@ class TresEnRaya {
             estado += "\n"
         }
         return estado
+    }
+
+    finalizado() {
+        if (comprobarHorizontales(this.tablero) ||
+            comprobarVerticales(this.tablero) || 
+            comprobarDiagonales(this.tablero))
+        {
+            return true
+        } else {
+            return false
+        }
     }
 }
 
--- partida.js
+++ partida.js
@@ -3,7 +3,11 @@ import TresEnRaya from './TresEnRaya.js'
 let partida = new TresEnRaya
 partida.jugar(1,1)
 partida.jugar(2,2)
-partida.jugar(0,1)
+partida.jugar(0,2)
+partida.jugar(2,1)
+partida.jugar(1,2)
+partida.jugar(2,0)
 
 console.log(partida.estado())
+console.log(partida.finalizado())
 
